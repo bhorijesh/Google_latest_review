7dd14d4406d86f128eb436ec03de870a4c30b65a
bhorijesh <rijeshbhochhibhoya@gmail.com>
2025-01-24 13:19:38 +0545

    updated

--- scrap.py
+++ scrap.py
@@ -8,12 +8,22 @@ from selenium.webdriver.support import expected_conditions as EC
 def setup_driver():
     """Initialize WebDriver with options."""
     options = webdriver.ChromeOptions()
+    # options.add_argument("--headless")
     driver = webdriver.Chrome(options=options)
     return driver
 
 def sort_reviews(driver):
     """Click the sort button and select the 'Newest' option."""
     try:
+        
+        try:
+            review_button = WebDriverWait(driver, 5).until(
+                EC.element_to_be_clickable((By.XPATH, "//button[@tabindex='-1']"))
+            )
+            review_button.click()
+        except Exception:
+            print("Review button not found, proceeding with sorting.")
+        #wait for Review to load
         sort_button = WebDriverWait(driver, 10).until(
             EC.element_to_be_clickable((By.XPATH, "//div[@class='IAbLGd']"))
         )
@@ -22,7 +32,6 @@ def sort_reviews(driver):
         newest_option = WebDriverWait(driver, 10).until(
             EC.element_to_be_clickable((By.XPATH, "//div[@class='fxNQSd' and @data-index='1']"))
         )
-
         newest_option.click()
     except Exception as e:
         print("Error clicking sort or newest option:", e)
@@ -39,7 +48,7 @@ def extract_reviewer_name(driver):
 
         # Save to CSV using pandas
         df = pd.DataFrame([{"Reviewer Name": reviewer_name}])
-        df.to_csv("latest_reviewer.csv", index=False, encoding="utf-8")
+        df.to_csv("latestreviewer.csv", index=False, encoding="utf-8")
 
     except Exception as e:
         print("Could not find the latest reviewer:", e)
@@ -48,7 +57,7 @@ def main():
 
     start = time.time()
     """Main function to execute the script."""
-    url = "https://www.google.com/maps/place/Camion+jo+pizza/@48.6159141,-1.4913629,17z/data=!4m18!1m9!3m8!1s0x89d4cb91e43a6ceb:0x1c880722f7dd12aa!2sAccess+Storage+-+Downtown+Toronto!8m2!3d43.661974!4d-79.31946!9m1!1b1!16s%2Fg%2F11bw420bj_!3m7!1s0x480eaf36fdfc5eeb:0x79df0e6acac777d2!8m2!3d48.6159141!4d-1.488788!9m1!1b1!16s%2Fg%2F11ry9gqbzc?hl=ne&entry=ttu&g_ep=EgoyMDI1MDEyMC4wIKXMDSoASAFQAw%3D%3D"
+    url = "https://maps.app.goo.gl/qxMxeropJE1P7G2x5"
     
     # Set up WebDriver
     driver = setup_driver()
